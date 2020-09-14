93157dfd3b4a342b7d875bb0eedcfd9ebc45ac0a
Rishi Gogia <rishigogia@gmail.com>
2020-09-14 13:08:55 +0100

    added equals method for comparisons

--- src/main/java/kata/supermarket/BuyOneGetOneFreeDiscount.java
+++ src/main/java/kata/supermarket/BuyOneGetOneFreeDiscount.java
@@ -3,6 +3,7 @@ package kata.supermarket;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
+import java.util.Objects;
 
 public class BuyOneGetOneFreeDiscount implements Discount {
 
@@ -14,5 +15,18 @@ public class BuyOneGetOneFreeDiscount implements Discount {
     @Override
     public BigDecimal getDiscountAmount(List<Item> itemList) {
         return BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final BuyOneGetOneFreeDiscount that = (BuyOneGetOneFreeDiscount) o;
+        return Objects.equals(discountCode, that.discountCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(discountCode);
     }
 }
