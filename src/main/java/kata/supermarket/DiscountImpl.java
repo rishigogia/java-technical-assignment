5f87f1acacf415a8ba87284a1e108bd47d05c306
Rishi Gogia <rishigogia@gmail.com>
2020-09-14 12:45:26 +0100

    Creating a discount implementation

---
+++ src/main/java/kata/supermarket/DiscountImpl.java
@@ -0,0 +1,11 @@
+package kata.supermarket;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class DiscountImpl implements Discount {
+    @Override
+    public BigDecimal getDiscountAmount(List<Item> itemList) {
+        return null;
+    }
+}
